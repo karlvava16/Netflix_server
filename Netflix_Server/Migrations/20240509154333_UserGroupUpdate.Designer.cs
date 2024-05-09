cf34f9bebb6df84635f9c86e721b7d5be6aee77a
Babchinsky <babchinsky.prog@gmail.com>
2024-05-09 18:45:00 +0300

    Добавил бэкап миграции UserGroup

--- Netflix_Server/Migrations/20240509154333_UserGroupUpdate.Designer.cs
+++ Netflix_Server/Migrations/20240509154333_UserGroupUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Netflix_Server.Models.Context;
 
@@ -11,9 +12,11 @@ using Netflix_Server.Models.Context;
 namespace Netflix_Server.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20240509154333_UserGroupUpdate")]
+    partial class UserGroupUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
