d6c32573bb9ac5ae55d8243278173c07c73a57c1
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-05-10 13:56:26 +0300

    Netflix

--- Netflix_Server/Models/MovieGroup/Movie.cs
+++ Netflix_Server/Models/MovieGroup/Movie.cs
@@ -4,12 +4,16 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public bool IsNetflix { get; set; }
-        public bool IsTop10 { get; set; }
+
+        public string? Director {  get; set; }
+
+        public string? Company {  get; set; }
+        public bool IsNetflix { get; set; } //Поверка
+        public bool IsTop10 { get; set; } //Проверка
         public string? Remark { get; set; }
         public string? Key { get; set; }
-        public string? StarRating { get; set; }
-        public string? Rating { get; set; }
+        public string? StarRating { get; set; } //Проверка
+        public string? Rating { get; set; } // проверка
         public string? Year { get; set; }
         public string? Runtime { get; set; }
 
