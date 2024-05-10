88a636a6cf4f89c565f2cfecd350cd06b2bda07f
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-05-10 16:04:00 +0300

    Netflix

--- Netflix_Server/Migrations/20240510122933_Update3.Designer.cs
+++ Netflix_Server/Migrations/20240510122933_Update3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Netflix_Server.Models.Context;
 
@@ -11,9 +12,11 @@ using Netflix_Server.Models.Context;
 namespace Netflix_Server.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20240510122933_Update3")]
+    partial class Update3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Netflix_Server/Migrations/20240510122933_Update3.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Netflix_Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Company",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Company",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Movies");
+        }
+    }
+}
